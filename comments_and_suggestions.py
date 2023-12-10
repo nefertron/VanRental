b78ea594d4473b26cff963d0e8fc92b32f5a9fd6
menard <130424612+nefertron@users.noreply.github.com>
2023-12-10 18:41:26 +0800

    modified landing page reviews section

---
+++ comments_and_suggestions.py
@@ -0,0 +1,36 @@
+
+# 1st  panel
+# -sa landing page dapat may gallery na makikita ex. Baguio tour and else
+# **** -sa pag popost ng van info dapat hindi lang daw picture ang makikita ng possible clients
+# **** -dapat daw may testimonial sections ng mga passenger
+# -route/tour analytics
+# -sales analytics
+# **** -massenges tab
+# -dapat daw nabibilang kung ilan yung na cacancel na rides
+# -pabigatin pa yung dashboard ng admin
+# -gumamit ng calendar api
+# -sa mga walk ins ( paano naman yung mga walk ins na gusto mag pa rent)
+
+
+# 2nd panel
+# **** -Admin can edit for price
+# **** -dapat sa pag rerent ng van kasama ang time kung kelan susunduin ang passenger wag lang daw date
+# **** -dapat sa posted van nakikita kung ilan ang seating capacity 
+# -driver dapat di na nakikita yung van rented  
+# -income month, year 
+# -sa mga walk ins
+
+# 3rd panel 
+# **** -Tanggalin na yung no. of services nan a view na passenger 
+# -admin  di na nakikita yung naka post na van
+# **** - tanggalin na muna yung naka set na price,, 
+# **** - dapat kung may naka rent nan a van sa day na yon di na pwedeng irent ng iba
+# -driver should have no conflict on their schedules
+# **** -dapat hanggang di pa mark as done ng driver di pa available yung van
+# **** - yung van dapat may pahinga din or maintenance
+# **** -calender – schedule of all reservations
+# - admin panel- focus on displaying important features
+# -admin di na rin nakikita yung van na naka post
+
+
+
